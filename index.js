e781b6db7e1d101e3938937808541f0d44c1b1a2
IanM21 <ianmcdonad2003@gmail.com>
2025-03-28 00:14:31 -0400

    CORS Updates for deployment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://friendship-plus.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
